73df94e221990996658cfc06f40801cde9b33f85
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-05 16:01:37 +0200

    style playlist grid component

--- src/components/PlaylistGrid.vue
+++ src/components/PlaylistGrid.vue
@@ -2,17 +2,24 @@
   <div class="playlist-grid-container">
 
     <div v-for="item in playlists" :key="item.name" class="playlist-grid-item">
-      {{ item.name }}
+      <div class="image">
+        <img :src="item.image" alt="">
+      </div>
+      <div class="name">
+        {{item.name}}
+      </div>
+      <ion-ripple-effect type="unbounded" />
     </div>
 
   </div>
 </template>
 
 <script lang="ts">
-
+import { IonRippleEffect, } from '@ionic/vue';
 
 export default {
   name: 'PlaylistGrid',
+  components: { IonRippleEffect },
   props: {
     playlists: null
   },
@@ -21,12 +28,45 @@ export default {
 
 <style lang="scss" scoped>
 .playlist-grid-container {
+  $item-space: 16px;
+  $item-size: 55px;
   display: flex;
   flex-wrap: wrap;
 
-  padding: 16px;
-}
-.playlist-grid-item {
-  flex: 50%;
+  padding: $item-space;
+
+  .playlist-grid-item {
+    position: relative;
+    display: flex;
+    flex: calc(50% - #{$item-space} / 2);
+    align-items: center;
+
+    margin: $item-space / 2 0;
+    height: $item-size;
+    border-radius: 8px;
+    overflow: hidden;
+    background-color: var(--ion-card-background);
+
+    &:nth-child(odd) {
+      margin-right: $item-space / 2;
+    }
+    &:nth-child(even) {
+      margin-left: $item-space / 2;
+    }
+
+    .image {
+      width: $item-size;
+      height: $item-size;
+      margin-right: 5px;
+
+      &> img {
+        width: 100%;
+      }
+    }
+    .name {
+      width: calc(100% - #{$item-size} - 5px);
+      font-weight: bolder;
+    }
+  }
 }
 </style>
