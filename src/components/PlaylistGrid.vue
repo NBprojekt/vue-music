8a3375754a95d8990b11264e63e510415e61d8a9
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-05 15:43:04 +0200

    create playlist grid

---
+++ src/components/PlaylistGrid.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="playlist-grid-container">
+
+    <div v-for="item in playlists" :key="item.name" class="playlist-grid-item">
+      {{ item.name }}
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+  name: 'PlaylistGrid',
+  props: {
+    playlists: null
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.playlist-grid-container {
+  display: flex;
+  flex-wrap: wrap;
+
+  padding: 16px;
+}
+.playlist-grid-item {
+  flex: 50%;
+}
+</style>
