3dc4751e402715caaccc6bd5e6f90c10668b8a9f
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-05 15:43:24 +0200

    load playlist vie service and display playlist gird

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,9 @@
           <ion-icon :icon="settingsOutline" size="large" slot="end" />
         </ion-toolbar>
       </ion-header>
+
+
+      <playlist-grid :playlists="playlists"/>
     
       <ExploreContainer name="Kurwa" />
     </ion-content>
@@ -19,21 +22,34 @@
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, } from '@ionic/vue';
 import { timeOutline, settingsOutline } from 'ionicons/icons';
+
+import { Playlists } from '@/types/playlist.type';
+
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import PlaylistGrid from '@/components/PlaylistGrid.vue';
+
+import DataService from '@/services/data.service';
+const dataService = new DataService();
 
 export default  {
   name: 'Home',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon, },
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon, PlaylistGrid },
   setup() {
     return {
       timeOutline,
       settingsOutline,
     }
   },
-  data() {
+  data(): any {
     return {
       title: 'Home',
+      playlists: [] as Playlists,
     }
+  },
+  async created() {
+    const playlists = await dataService.getPlaylists();
+    console.log('Playlists', playlists);
+    this.playlists = playlists;
   },
 }
 </script>
