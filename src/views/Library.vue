256128a4ffc7740f123ced566cfab5cda59b7825
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-07 12:43:38 +0200

    add segmetns

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -10,7 +10,23 @@
         </ion-toolbar>
       </ion-header>
     
-      <ExploreContainer name="Kurwa" />
+      <div class="static-segments">
+        <ion-segment @ionChange="segmentChanged($event)" :value="selectedTab">
+          <ion-segment-button value="playlists">
+            <ion-label>Playlists</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="artists">
+            <ion-label>Artists</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="albums">
+            <ion-label>Albums</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </div>
+
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
     </ion-content>
 
   </ion-page>
@@ -18,13 +34,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonSegment, IonLabel, IonSegmentButton, } from '@ionic/vue';
 import { searchOutline, addOutline } from 'ionicons/icons';
-import ExploreContainer from '@/components/explore-container.component.vue';
 
 export default defineComponent({
   name: 'Library',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon, },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon, IonSegment, IonLabel, IonSegmentButton, },
   setup() {
     return {
       searchOutline,
@@ -34,7 +49,30 @@ export default defineComponent({
   data() {
     return {
       title: 'Library',
+      selectedTab: 'playlists',
     }
   },
+  methods: {
+    segmentChanged(event: CustomEvent) {
+      this.selectedTab = event.detail.value;
+      console.log(`Selected tab changed: ${this.selectedTab}`, event);
+    }
+  }
 });
 </script>
+
+<style lang="scss" scoped>
+
+.static-segments {
+  position: sticky;
+  top: 16px;
+  padding: 10px 0;
+  background: var(--ion-background-color);
+
+  ion-segment {
+    margin: 0 16px;
+    width: calc(100% - 16px * 2);
+  }
+}
+
+</style>
