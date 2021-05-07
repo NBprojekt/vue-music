ac94284051603bedde41e71a16035bd6e97e8e59
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-07 11:06:10 +0200

    map and sort playlists after fetching

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -4,11 +4,17 @@ import { Playlists } from "@/types/playlist.type";
 export class DataService {
 
     public async getPlaylists(): Promise<Playlists> {
-        const response = await fetch('./assets/data/playlists.json');
-        const playlists = await response.json();
-        
-        console.log('Fetched playlists', playlists);
+        // Fetch json file
+        const response: Response = await fetch('./assets/data/playlists.json');
+        const playlists: Playlists = await response.json();
 
+        // Map iso string to date object
+        playlists.map(x => x.lastPlayed = new Date(x.lastPlayed));
+
+        // Order playlists desc by last played date
+        playlists.sort((a, b) => a.lastPlayed < b.lastPlayed ? 1 : -1);
+
+        console.log('Fetched playlists', playlists);
         return playlists;
     }
 }
