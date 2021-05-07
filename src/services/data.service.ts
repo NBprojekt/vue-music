8a06ba44f8b708bb7dac917bd00e2dc4a9eb6cbb
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-07 16:13:36 +0200

    add getArtists() function

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -17,6 +17,21 @@ export class DataService {
         console.log('Fetched playlists', playlists);
         return playlists;
     }
+
+    public async getArtists(): Promise<Playlists> {
+        // Fetch json file
+        const response: Response = await fetch('./assets/data/artists.json');
+        const playlists: Playlists = await response.json();
+
+        // Map iso string to date object
+        playlists.map(x => x.lastPlayed = new Date(x.lastPlayed));
+
+        // Order playlists desc by last played date
+        playlists.sort((a, b) => a.lastPlayed < b.lastPlayed ? 1 : -1);
+
+        console.log('Fetched artists', playlists);
+        return playlists;
+    }
 }
 
 export const dataService = new DataService();
