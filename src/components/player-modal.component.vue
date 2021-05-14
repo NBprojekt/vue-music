63c573d6a0f6c23bbc93e401d48e6c13d320d284
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-14 10:18:59 +0200

    create player modal

---
+++ src/components/player-modal.component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    Player modal
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'PlayerModal',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
