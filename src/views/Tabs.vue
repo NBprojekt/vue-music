64cb9934315a0cf6452a5838eade5c9fb4593683
Norbert Bartko <bartko.norbert@gmail.com>
2021-05-20 13:45:55 +0200

    open player modal in full screen

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -56,6 +56,7 @@ export default defineComponent({
       const modal = await modalController.create({
         component: PlayerModal,
         swipeToClose: true,
+        cssClass: 'fullscreen',
       });
       return modal.present();
     }
