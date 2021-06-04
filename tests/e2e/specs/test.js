e2807b4ce63e73d27a4ff6048ec2415092739d84
Norbert Bartko <bartko.norbert@gmail.com>
2021-06-04 16:33:18 +0200

    add some basic test that run successfuly :D

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,8 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('ion-content', 'Tab 1 page')
+    cy.contains('ion-tabs', 'Home')
+    cy.contains('ion-tabs', 'Search')
+    cy.contains('ion-tabs', 'Library')
   })
 })
