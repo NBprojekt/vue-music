c08c1e2c1f84742892050c76b433e25ff22211e1
Norbert Bartko <bartko.norbert@gmail.com>
2021-06-07 12:01:29 +0200

    playerservice provides current song as observable

--- src/services/player.service.ts
+++ src/services/player.service.ts
@@ -1,20 +1,32 @@
 import { Song } from "@/types/song.type";
+import { BehaviorSubject, Observable } from "rxjs";
 
 
 export class PlayerService {
+  private currentSong: BehaviorSubject<Song | null>;
 
-    public getCurrentSong(): Song {
-        return {
-          uid: '123a3sHn64dGer',
-          title: 'In the End',
-          author: 'Linking Park',
-          file: '',
-          length: 240,
-          image: `${process.env.VUE_APP_BASE_HREF}assets/images/playlist_1.jpg`,
-          // canvas: `${process.env.VUE_APP_BASE_HREF}assets/videos/test.gif`,
-        };
-    }
+  constructor() {
+    this.currentSong = new BehaviorSubject<Song | null>(null);
 
+    // For development
+    this.setSong({
+      uid: '123a3sHn64dGer',
+      title: 'In the End',
+      author: 'Linking Park',
+      file: '',
+      length: 240,
+      image: `${process.env.VUE_APP_BASE_HREF}assets/images/playlist_1.jpg`,
+      // canvas: `${process.env.VUE_APP_BASE_HREF}assets/videos/test.gif`,
+    });
+  }
+
+  public setSong(song: Song): void {
+    this.currentSong.next(song);
+  }
+
+  public getSong(): Observable<Song | null> {
+    return this.currentSong.asObservable();
+  }
 }
 
 export const playerService = new PlayerService();
