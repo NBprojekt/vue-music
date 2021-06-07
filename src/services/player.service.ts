3239a36fcf21517f627dcc656f7f101a9cd4c4fb
Norbert Bartko <bartko.norbert@gmail.com>
2021-06-07 13:35:50 +0200

    update player service to hold current controlls values

--- src/services/player.service.ts
+++ src/services/player.service.ts
@@ -5,8 +5,18 @@ import { BehaviorSubject, Observable } from "rxjs";
 export class PlayerService {
   private currentSong: BehaviorSubject<Song | null>;
 
+  private _isPlaying: BehaviorSubject<boolean>;
+  private _isShuffling: BehaviorSubject<boolean>;
+  private _isRepeating: BehaviorSubject<boolean>;
+  private _isRepeatingOnlyOne: BehaviorSubject<boolean>;
+
   constructor() {
     this.currentSong = new BehaviorSubject<Song | null>(null);
+
+    this._isPlaying = new BehaviorSubject<boolean>(false);
+    this._isShuffling = new BehaviorSubject<boolean>(false);
+    this._isRepeating = new BehaviorSubject<boolean>(false);
+    this._isRepeatingOnlyOne = new BehaviorSubject<boolean>(false);
 
     // For development
     this.setSong({
@@ -23,9 +33,39 @@ export class PlayerService {
   public setSong(song: Song): void {
     this.currentSong.next(song);
   }
-
   public getSong(): Observable<Song | null> {
     return this.currentSong.asObservable();
+  }
+
+  public togglePlaying(): void {
+    this._isPlaying.next(!this._isPlaying.value);
+  }
+  public toggleShuffle(): void {
+    this._isShuffling.next(!this._isShuffling.value);
+  }
+  public toggleRepeat(): void {
+
+    if (!this._isRepeating.value) {
+      this._isRepeating.next(true);
+    } else if (!this._isRepeatingOnlyOne.value) {
+      this._isRepeatingOnlyOne.next(true);
+    } else {
+      this._isRepeating.next(false);
+      this._isRepeatingOnlyOne.next(false);
+    }
+  }
+
+  public isPlaying(): Observable<boolean> {
+    return this._isPlaying.asObservable();
+  }
+  public isShuffling(): Observable<boolean> {
+    return this._isShuffling.asObservable();
+  }
+  public isRepeating(): Observable<boolean> {
+    return this._isRepeating.asObservable();
+  }
+  public isRepeatingOnlyOne(): Observable<boolean> {
+    return this._isRepeatingOnlyOne.asObservable();
   }
 }
 
