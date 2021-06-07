da6f6d9c80e94c6c3fe76d739b445a5699298fb9
Norbert Bartko <bartko.norbert@gmail.com>
2021-06-07 16:41:22 +0200

    add optional albump for artists

--- src/types/artist.type.ts
+++ src/types/artist.type.ts
@@ -1,10 +1,12 @@
-import { Songs } from "./song.type";
+import { Albums } from './album.type';
+import { Songs } from './song.type';
 
 export type Artist = {
-    name: string;
-    image: string;
-    lastPlayed: Date;
-    songs: Songs;
+  name: string;
+  image: string;
+  lastPlayed: Date;
+  songs?: Songs;
+  albums?: Albums;
 }
 
 export type Artists = Array<Artist>;
